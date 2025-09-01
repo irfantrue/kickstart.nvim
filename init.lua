787aa4bbaa34ea67d8b0b8c2324567e68e828c10
Irfan Nurul Susilo <irfan.nurul.susilo@proton.me>
2025-09-01 11:56:35 +0700

    Update init.lua

--- init.lua
+++ init.lua
@@ -1127,6 +1127,32 @@ require('lazy').setup({
                     theme = 'auto',
                     component_separators = { left = '│', right = '│' },
                     section_separators = { left = '', right = '' },
+                    disabled_filetypes = {
+                        statusline = {},
+                        winbar = {},
+                    },
+                    ignore_focus = {},
+                    always_divide_middle = true,
+                    always_show_tabline = true,
+                    globalstatus = false,
+                    refresh = {
+                        statusline = 1000,
+                        tabline = 1000,
+                        winbar = 1000,
+                        refresh_time = 16, -- ~60fps
+                        events = {
+                            'WinEnter',
+                            'BufEnter',
+                            'BufWritePost',
+                            'SessionLoadPost',
+                            'FileChangedShellPost',
+                            'VimResized',
+                            'Filetype',
+                            'CursorMoved',
+                            'CursorMovedI',
+                            'ModeChanged',
+                        },
+                    },
                 },
                 sections = {
                     lualine_a = {
@@ -1151,7 +1177,23 @@ require('lazy').setup({
                     },
                     lualine_b = { 'branch', 'diff' },
                     lualine_c = { 'filename' },
-                    lualine_x = { 'filetype' },
+                    lualine_x = {
+                        'filetype',
+                        {
+                            'lsp_status',
+                            -- icon = '', -- f013
+                            symbols = {
+                                -- Standard unicode symbols to cycle through for LSP progress:
+                                spinner = { '⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏' },
+                                -- Standard unicode symbol for when LSP is done:
+                                done = '✓',
+                                -- Delimiter inserted between LSP names:
+                                separator = ' ',
+                            },
+                            -- List of LSP names to ignore (e.g., `null-ls`):
+                            ignore_lsp = {},
+                        },
+                    },
                     lualine_y = { 'progress' },
                     lualine_z = { 'location' },
                 },
